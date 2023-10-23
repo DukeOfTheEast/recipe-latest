e40db7dfcde6725bbdef2a6c45e8da0e517ce4c6
Ukachukwu Ebuka <fralexander1999@gmail.com>
2023-10-23 13:36:10 +0100

    image

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import RecipeList from "./components/RecipeList";
 import Footer from "./components/Footer";
+import Vision from "./components/Vision";
 
 function App() {
   return (
     <div className="App">
       <h2 className="logo">MyRecipe</h2>
+      <Vision />
       <RecipeList />
       <Footer />
     </div>
---
+++ src/components/Vision.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ingredient from "../images/ingredients.jpg";
+
+const Vision = () => {
+  return (
+    <div className="vision">
+      <h1>We serve you and also give you the allowance to serve yourselves</h1>
+      <img src={Ingredient} alt="" />
+    </div>
+  );
+};
+
+export default Vision;
